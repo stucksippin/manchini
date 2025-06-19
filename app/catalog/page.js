18f49d54487b744cab54525139beb1469e0b7b83
danivelin <113595467+stucksippin@users.noreply.github.com>
2025-06-19 22:15:32 +0300

    poshla zhara

--- app/catalog/page.js
+++ app/catalog/page.js
@@ -3,17 +3,21 @@ export const dynamic = 'force-dynamic';
 import Catalog from '@/components/Catalog';
 import Filter from '@/components/Filter';
 import { getObjects } from '@/libs/getObject';
-import { getPriceAreaLimits } from '@/libs/getPriceAreaLimits';
-
 
 export default async function CatalogPage({ searchParams }) {
-    console.log(searchParams);
+    // «Материализуем» async‑поля
+    const city = await searchParams.city || '';
+    const type = await searchParams.type || '';
+    const rooms = await searchParams.rooms || '';
+    const priceMin = await searchParams.priceMin || '';
+    const priceMax = await searchParams.priceMax || '';
 
-    const objects = await getObjects(searchParams);
-    // const limits = await getPriceAreaLimits()
+    // Передаём в getObjects чистые строки
+    const objects = await getObjects({ city, type, rooms, priceMin, priceMax });
+
     return (
         <div className="flex mt-[2%] mb-[2%] gap-x-10 mx-[5%]">
-            <Filter searchParams={searchParams} />
+            <Filter searchParams={{ city, type, rooms, priceMin, priceMax }} />
             <Catalog objects={objects} />
         </div>
     );
--- libs/getObject.js
+++ libs/getObject.js
@@ -1,31 +1,20 @@
 import prisma from './prisma';
 
-export async function getObjects(searchParams) {
+export async function getObjects(filters) {
+    const { city, type, rooms, priceMin, priceMax } = filters;
+
     try {
         const objects = await prisma.realty.findMany({
             where: {
                 AND: [
-                    searchParams.city ? { city: searchParams.city } : {},
-                    searchParams.type
-                        ? {
-                            type: {
-                                in: searchParams.type.split(','),
-                            },
-                        }
-                        : {},
-                    searchParams.rooms
-                        ? {
-                            rooms: {
-                                in: searchParams.rooms.split(',').map(Number),
-                            },
-                        }
-                        : {},
-                    searchParams.priceMax ? { price: { lte: Number(searchParams.priceMax) } } : {},
-                ]
+                    city ? { city } : {},
+                    type ? { type: { in: type.split(',') } } : {},
+                    rooms ? { rooms: { in: rooms.split(',').map(Number) } } : {},
+                    priceMin ? { price: { gte: Number(priceMin) } } : {},
+                    priceMax ? { price: { lte: Number(priceMax) } } : {},
+                ],
             },
-            include: {
-                photos: true,
-            },
+            include: { photos: true },
         });
         return objects;
     } catch (error) {
